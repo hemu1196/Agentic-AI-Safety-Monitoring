9bc90880b6ba640ca5c83bd7f23dbbb37afd0fee
Hema Chandra <hemachandra@Hemas-MacBook-Air.local>
2026-08-13 17:33:49 +0530

    Fix NameError: import pandas as pd in drone_cv_safety.py

--- app_modules/drone_cv_safety.py
+++ app_modules/drone_cv_safety.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
+import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 import plotly.express as px
 import plotly.graph_objects as go
